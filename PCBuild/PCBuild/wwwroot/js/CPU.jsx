8acd0f97e95c93b1c7259920d5a06199b75fa08d
MiuCatalinStefan <catalinmiu1998@yahoo.ro>
2020-12-21 12:58:40 +0200

    Inceracre creeare casute componente

---
+++ PCBuild/PCBuild/wwwroot/js/CPU.jsx
@@ -0,0 +1,9 @@
+﻿class CommentBox extends React.Component {
+    render() {
+        return (
+            <div className="CPUBox">Aici Vine CPU-ul!</div>
+        );
+    }
+}
+
+ReactDOM.render(<CommentBox />, document.getElementById('content'));
